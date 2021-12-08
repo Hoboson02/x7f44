c2b1667898fedf0f9900f241d402451073afbf65
Ryan Earl <ryanleoearl@gmail.com>
2021-12-08 11:57:25 -0800

    Renamed variable name again to be easier to understand and refactored the die initializer to use args.

--- die.rb
+++ die.rb
@@ -2,16 +2,19 @@ class Die
 
   attr_reader :number_of_sides
   attr_accessor :rolled_value
-  attr_reader :random_value
+  attr_reader :chosen_number_of_sides
 
-  def initialize(number_of_sides)
-    @number_of_sides = number_of_sides
-    @rolled_value = 1
-    @random_value = Random.new
+  INITIAL_VALUE = 1
+  NUM_SIDES = 6
+
+  def initialize(args)
+    @number_of_sides = args[:number_of_sides] || NUM_SIDES
+    @rolled_value = args[:rolled_value] || INITIAL VALUE
+    @chosen_number_of_sides = args [:chosen_number_of_sides] || Random.new
   end
 
   def roll!
-    self.rolled_value = random_value.rand(1..number_of_sides)
+    self.rolled_value = chosen_number_of_sides.rand(1..number_of_sides)
   end
 
   def makeRoll
